af1e373c041e68e0b90c22ab6c48a425a83c6905
John Olav Lund <john.olav.lund@gmail.com>
2018-01-20 10:49:13 +0100

    More work on the HTTP parser
    Need to test the correctness and efficiency of the implementation.

--- decoder/packet_http.c
+++ decoder/packet_http.c
@@ -4,22 +4,41 @@
 #include "packet.h"
 #include "packet_http.h"
 
-#define MAX_HTTP_LINE 4096
+/* It is recommended that all HTTP senders and recipients support, at a minimum,
+   request-line lengths of 8000 octets, cf. RFC 7230 */
+#define MAX_HTTP_LINE 8000
 
-static enum http_state {
+#define VERSION_LEN 8
+
+enum header_state {
     FIELD,
     VAL,
     EOL
-} state;
+};
+
+static char *request_method[] = {
+    "CONNECT",
+    "DELETE",
+    "GET",
+    "HEAD",
+    "OPTIONS",
+    "POST",
+    "PUT",
+    "TRACE"
+};
+
+#define NUM_METHODS sizeof(request_method) / sizeof(char *)
 
 static bool parse_http(unsigned char *buf, uint16_t len, struct http_info *http);
+static bool parse_start_line(unsigned char **str, unsigned int *len, struct http_info *http);
+static bool check_method(char *token);
 static bool parse_http_header(unsigned char **str, unsigned int *len, list_t **header);
 
 packet_error handle_http(unsigned char *buffer, uint16_t len, struct application_info *info)
 {
     info->http = calloc(1, sizeof(struct http_info));
     if (!parse_http(buffer, len, info->http)) {
-        return HTTP_ERR;
+        return UNK_PROTOCOL;
     }
     return NO_ERR;
 }
@@ -32,44 +51,21 @@ packet_error handle_http(unsigned char *buffer, uint16_t len, struct application
 bool parse_http(unsigned char *buffer, uint16_t len, struct http_info *http)
 {
     unsigned char *ptr;
-    char line[MAX_HTTP_LINE];
     bool is_http = false;
-    int i;
-    int n;
+    unsigned int n;
 
-    i = 0;
     n = len;
     ptr = buffer;
-
-    /* parse start line */
-    while (isascii(*ptr)) {
-        if (i > len || i > MAX_HTTP_LINE) return false;
-        if (*ptr == '\r') {
-            if (*++ptr == '\n') {
-                ptr++;
-                is_http = true;
-                break;
-            } else {
-                return false;
-            }
-        }
-        line[i++] = *ptr++;
+    if (!parse_start_line(&ptr, &n, http)) {
+        return false;
     }
-    if (!is_http) return false;
-    line[i] = '\0';
-    http->start_line = strdup(line);
 
     /* parse header fields */
-    unsigned int header_len;
-
     http->header = list_init();
-    n -= (i + 2); /* subtract i + CRLF */
-    header_len = n;
-    is_http = parse_http_header(&ptr, &header_len, &http->header);
+    is_http = parse_http_header(&ptr, &n, &http->header);
 
     /* copy message body */
     if (is_http) {
-        n -= header_len;
         if (n) {
             http->data = malloc(n);
             memcpy(http->data, ptr, n);
@@ -81,11 +77,76 @@ bool parse_http(unsigned char *buffer, uint16_t len, struct http_info *http)
     return is_http;
 }
 
+bool parse_start_line(unsigned char **str, unsigned int *len, struct http_info *http)
+{
+    unsigned int i = 0;
+    unsigned char *ptr = *str;
+    char line[MAX_HTTP_LINE];
+
+    while (isprint(*ptr) && *ptr != ' ') {
+        if (i > VERSION_LEN || i > *len) {
+            *str = ptr;
+            return false;
+        }
+        line[i++] = *ptr++;
+    }
+    if (*ptr++ == ' ') {
+        line[i] = '\0';
+        if (check_method(line) ||
+            (*len > VERSION_LEN && strncmp(line, "HTTP/1.1", VERSION_LEN)) == 0) {
+            line[i++] = ' ';
+            while (isascii(*ptr)) {
+                if (i > *len || i > MAX_HTTP_LINE) {
+                    *str = ptr;
+                    return false;
+                }
+                if (*ptr == '\r') {
+                    if (*++ptr == '\n') {
+                        ptr++;
+                        line[i] = '\0';
+                        http->start_line = strdup(line);
+                        *len -= (i + 2); /* start_line + CRLF */
+                        *str = ptr;
+                        return true;
+                    } else {
+                        *str = ptr;
+                        return false;
+                    }
+                }
+                line[i++] = *ptr++;
+            }
+        }
+    }
+    *str = ptr;
+    return false;
+}
+
+bool check_method(char *token)
+{
+    int low = 0;
+    int high = NUM_METHODS - 1;
+    int mid;
+    int c;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+        c = strcmp(token, request_method[mid]);
+        if (c == 0) {
+            return true;
+        } else if (c < 0) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return false;
+}
+
 /*
  * Parses the HTTP header and stores the lines containg the field and the value
  * in a list. "len" is a value-result argument and its value is the length of
- * the str argument. On return, the length of the header is stored in len (or
- * where it failed in case of error).
+ * the str argument. On return, the length of the header is subtracted from len
+ * (or where it failed in case of error).
  *
  * Returns the result of the operation.
  */
@@ -97,6 +158,7 @@ bool parse_http_header(unsigned char **str, unsigned int *len, list_t **header)
     unsigned char *ptr = *str;
     char line[MAX_HTTP_LINE];
     int n = *len;
+    enum header_state state;
 
     for (i = 0; i < n && !eoh && is_http; i += j) {
         int c = 0;
@@ -105,7 +167,7 @@ bool parse_http_header(unsigned char **str, unsigned int *len, list_t **header)
         is_http = false;
         for (j = 0; !is_http && !eoh && isascii(*ptr) && j + i < n; j++, ptr++) {
             if (c > MAX_HTTP_LINE) {
-                *len = i + j;
+                *len -= (i + j);
                 *str = ptr;
                 return false;
             }
@@ -129,12 +191,12 @@ bool parse_http_header(unsigned char **str, unsigned int *len, list_t **header)
                 break;
             case EOL:
                 if (*ptr != '\n') {
-                    *len = i + j;
+                    *len -= (i + j);
                     *str = ptr;
                     return false;
                 }
                 if (j == 1) { /* end of header fields */
-                    *len = i + j + 1;
+                    *len -= (i + j + 1);
                     *str = ptr + 1;
                     return true;
                 } else {
@@ -146,7 +208,7 @@ bool parse_http_header(unsigned char **str, unsigned int *len, list_t **header)
             }
         }
     }
-    *len = i;
+    *len -= i;
     *str = ptr;
     return false;
 }
