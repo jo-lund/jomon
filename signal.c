2e87e277efc053c81df8218cc12e1d65e563d952
John Olav Lund <john.olav.lund@gmail.com>
2018-02-17 13:44:41 +0100

    Added more functionality to the publisher/subscriber
    This needs more work

--- signal.c
+++ signal.c
@@ -3,42 +3,69 @@
 #include "list.h"
 
 struct publisher {
-    list_t *subscriptions;
+    list_t *subscriptions0;
+    list_t *subscriptions1;
 };
 
 publisher_t *publisher_init()
 {
     publisher_t *p = malloc(sizeof(publisher_t));
 
-    p->subscriptions = list_init();
+    p->subscriptions0 = list_init();
+    p->subscriptions1 = list_init();
     return p;
 }
 
 void publisher_free(publisher_t *p)
 {
-    list_free(p->subscriptions, NULL);
+    list_free(p->subscriptions0, NULL);
+    list_free(p->subscriptions1, NULL);
     free(p);
 }
 
-void add_subscription(publisher_t *p, function f)
+void add_subscription0(publisher_t *p, publisher_fn0 f)
 {
-    list_push_back(p->subscriptions, f);
+    list_push_back(p->subscriptions0, f);
 }
 
-void remove_subscription(publisher_t *p, function f)
+void remove_subscription0(publisher_t *p, publisher_fn0 f)
 {
-    list_remove(p->subscriptions, f, NULL);
+    list_remove(p->subscriptions0, f, NULL);
 }
 
-void publish(publisher_t *p)
+void publish0(publisher_t *p)
 {
-    const node_t *n = list_begin(p->subscriptions);
+    const node_t *n = list_begin(p->subscriptions0);
 
     while (n) {
-        function func = list_data(n);
+        publisher_fn0 func = list_data(n);
 
         if (func) {
             func();
+        }
+        n = list_next(n);
+    }
+}
+
+void add_subscription1(publisher_t *p, publisher_fn1 f)
+{
+    list_push_back(p->subscriptions1, f);
+}
+
+void remove_subscription1(publisher_t *p, publisher_fn1 f)
+{
+    list_remove(p->subscriptions1, f, NULL);
+}
+
+void publish1(publisher_t *p, void *d)
+{
+    const node_t *n = list_begin(p->subscriptions1);
+
+    while (n) {
+        publisher_fn1 func = list_data(n);
+
+        if (func) {
+            func(d);
         }
         n = list_next(n);
     }
--- signal.h
+++ signal.h
@@ -2,12 +2,16 @@
 #define SIGNAL_H
 
 typedef struct publisher publisher_t;
-typedef void (*function)(void);
+typedef void (*publisher_fn0)();
+typedef void (*publisher_fn1)(void *);
 
 publisher_t *publisher_init();
 void publisher_free(publisher_t *p);
-void add_subscription(publisher_t *p, function f);
-void remove_subscription(publisher_t *p, function f);
-void publish(publisher_t *p);
+void add_subscription0(publisher_t *p, publisher_fn0 f);
+void remove_subscription0(publisher_t *p, publisher_fn0 f);
+void publish0(publisher_t *p);
+void add_subscription1(publisher_t *p, publisher_fn1 f);
+void remove_subscription1(publisher_t *p, publisher_fn1 f);
+void publish1(publisher_t *p, void *data);
 
 #endif
