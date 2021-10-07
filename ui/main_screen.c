jo-lund/jomon

commit 2f9046a07eef6f6bcbe7074267456e449f034044

Cleaned up main_screen_refresh
Will now always redraw the main screen on screen changes. This fixes some
isues with resize of the terminal. Also updated the actionbar on connection
screen changes.

ui/main_screen.c
@@ -181,40 +181,24 @@ void main_screen_refresh(screen *s)
 {
     int my;
     main_screen *ms;
-    int c;
+    int y;
 
     ms = (main_screen *) s;
     my = getmaxy(s->win);
     wbkgd(s->win, get_theme_colour(BACKGROUND));
     wbkgd(ms->header, get_theme_colour(BACKGROUND));
     touchwin(s->win);
-    c = vector_size(ms->packet_ref) - 1;
-
-    /* re-render the whole screen when capturing or resizing */
     if (s->resize) {
-        int y;
         int x;
 
         getmaxyx(stdscr, y, x);
         mvwin(status, y - 1, 0);
         y = y - HEADER_HEIGHT - actionbar_getmaxy(actionbar);
         wresize(s->win, y, x);
-        ms->outy = print_lines(ms, ms->base.top, ms->base.top + y, 0);
-    } else if (!s->show_selectionbar && (ms->outy >= my || c >= my) && ctx.capturing) {
-        print_packets(ms);
-    } else if (ctx.capturing && ms->outy > 0 && ms->outy < my) {
-        int i;
-
-        for (i = ms->outy; i < my && i <= c; i++) {
-            struct packet *p;
-            char buffer[MAXLINE];
-
-            p = vector_get_data(ms->packet_ref, i);
-            write_to_buf(buffer, MAXLINE, p);
-            printnlw(s->win, buffer, strlen(buffer), i, 0, ms->scrollx);
-        }
-        ms->outy = i;
+    } else {
+        y = my;
     }
+    ms->outy = print_lines(ms, ms->base.top, ms->base.top + y, 0);
     if (s->show_selectionbar) {
         show_selectionbar(ms, s->win, s->selectionbar - s->top, A_NORMAL);
     }

ui/connection_screen.c
@@ -198,6 +198,7 @@ void connection_screen_render(connection_screen *cs)
     touchwin(cs->base.win);
     print_conn_header(cs);
     print_all_connections(cs);
+    actionbar_refresh(actionbar, (screen *) cs);
 }
 
 void update_connection(struct tcp_connection_v4 *conn, bool new_connection)
@@ -227,6 +228,7 @@ void update_connection(struct tcp_connection_v4 *conn, bool new_connection)
             y++;
         }
     }
+    actionbar_refresh(actionbar, (screen *) cs);
 }
 
 void print_conn_header(connection_screen *cs)
@@ -274,7 +276,6 @@ void print_connection(connection_screen *cs, struct tcp_connection_v4 *conn, int
     memset(entry, 0, sizeof(entry));
     inet_ntop(AF_INET, &conn->endp->src, entry[ADDRA].buf, INET_ADDRSTRLEN);
     inet_ntop(AF_INET, &conn->endp->dst, entry[ADDRB].buf, INET_ADDRSTRLEN);
-    p = list_data(n);
     entry[ADDRA].val = conn->endp->src;
     entry[PORTA].val = conn->endp->sport;
     entry[ADDRB].val = conn->endp->dst;

commit d943c0f8a060cefde7bdef5963939d7606c03cd4

Fixed a regression introduced by 2f9046a
Need to handle an active scan specifically

ui/main_screen.c
@@ -181,7 +181,6 @@ void main_screen_refresh(screen *s)
 {
     int my;
     main_screen *ms;
-    int y;
 
     ms = (main_screen *) s;
     my = getmaxy(s->win);
@@ -189,19 +188,19 @@ void main_screen_refresh(screen *s)
     wbkgd(ms->header, get_theme_colour(BACKGROUND));
     touchwin(s->win);
     if (s->resize) {
-        int x;
+        int y, x;
 
         getmaxyx(stdscr, y, x);
         mvwin(status, y - 1, 0);
-        y = y - HEADER_HEIGHT - actionbar_getmaxy(actionbar);
-        wresize(s->win, y, x);
-    } else {
-        y = my;
+        my = y - HEADER_HEIGHT - actionbar_getmaxy(actionbar);
+        wresize(s->win, my, x);
     }
-    ms->outy = print_lines(ms, ms->base.top, ms->base.top + y, 0);
-    if (s->show_selectionbar) {
+    if (!s->show_selectionbar && ctx.capturing && vector_size(ms->packet_ref) > my)
+        print_packets(ms);
+    else
+        ms->outy = print_lines(ms, ms->base.top, ms->base.top + my, 0);
+    if (s->show_selectionbar)
         show_selectionbar(ms, s->win, s->selectionbar - s->top, A_NORMAL);
-    }
     wnoutrefresh(s->win);
     if (ms->subwindow.win) {
         struct packet *p;
@@ -408,7 +407,6 @@ void main_screen_get_input(screen *s)
         break;
     case 'e':
     case KEY_F(8):
-        DEBUG("%s F8/e", __func__);
         input_mode = (input_mode == INPUT_FILTER) ? INPUT_NONE : INPUT_FILTER;
         handle_input_mode(ms, "Filter: ");
         break;