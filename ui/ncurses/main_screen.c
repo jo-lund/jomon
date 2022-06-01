jo-lund/jomon

commit 1bd2e87c8643fc81e3c79a0370484086870095d8

Don't print a generic error message for protocols above the datalink layer
TODO: when there's an error in decoding the protocol it should be shown
where the error is.

ui/print_protocol.c
@@ -46,24 +46,20 @@ static void print_error(char *buf, int size, struct packet *p);
 
 void write_to_buf(char *buf, int size, struct packet *p)
 {
-    if (p->perr != NO_ERR && p->perr != UNK_PROTOCOL) {
+    struct protocol_info *pinfo = NULL;
+
+    if (p->root->next)
+        pinfo = get_protocol(p->root->next->id);
+    if (pinfo) {
+        char time[TBUFLEN];
+        struct timeval t = p->time;
+
+        format_timeval(&t, time, TBUFLEN);
+        PRINT_NUMBER(buf, size, p->num);
+        PRINT_TIME(buf, size, time);
+        pinfo->print_pdu(buf, size, p);
+    } else if (p->len - ETHER_HDR_LEN)
         print_error(buf, size, p);
-    } else {
-        struct protocol_info *pinfo = NULL;
-
-        if (p->root->next)
-            pinfo = get_protocol(p->root->next->id);
-        if (pinfo) {
-            char time[TBUFLEN];
-            struct timeval t = p->time;
-
-            format_timeval(&t, time, TBUFLEN);
-            PRINT_NUMBER(buf, size, p->num);
-            PRINT_TIME(buf, size, time);
-            pinfo->print_pdu(buf, size, p);
-        } else if (p->len - ETHER_HDR_LEN)
-            print_error(buf, size, p);
-    }
 }
 
 static void print_error(char *buf, int size, struct packet *p)

commit 571fced04af24b406497374912ffccc922999d64

Fixed a crash when packet data is empty
This fixes a regression introduced by 1bd2e87

ui/ncurses/main_screen.c
@@ -1212,8 +1212,10 @@ void add_elements(main_screen *ms, struct packet *p)
         pinfo = get_protocol(pdata->id);
         idx += pdata->len;
         if (pinfo && i < NUM_LAYERS) {
-            header = LV_ADD_HEADER(ms->lvw, pinfo->long_name, selected[i], i);
-            pinfo->add_pdu(ms->lvw, header, pdata);
+            if (pdata && pdata->data) {
+                header = LV_ADD_HEADER(ms->lvw, pinfo->long_name, selected[i], i);
+                pinfo->add_pdu(ms->lvw, header, pdata);
+            }
         }
         i++;
         pdata = pdata->next;

ui/print_protocol.c
@@ -197,9 +197,7 @@ void print_llc(char *buf, int n, void *data)
     HW_ADDR_NTOP(smac, eth_src(p));
     HW_ADDR_NTOP(dmac, eth_dst(p));
     PRINT_ADDRESS(buf, n, smac, dmac);
-    if (pdata->next)
-        pinfo = get_protocol(pdata->next->id);
-    if (pinfo) {
+    if (pdata->next && pdata->next->data && (pinfo = get_protocol(pdata->next->id))) {
         pinfo->print_pdu(buf, n, pdata->next);
     } else {
         PRINT_PROTOCOL(buf, n, "LLC");
@@ -287,9 +285,7 @@ void print_ipv6(char *buf, int n, void *data)
     inet_ntop(AF_INET6, ip->src, src, INET6_ADDRSTRLEN);
     inet_ntop(AF_INET6, ip->dst, dst, INET6_ADDRSTRLEN);
     PRINT_ADDRESS(buf, n, src, dst);
-    if (pdata->next)
-        pinfo = get_protocol(pdata->next->id);
-    if (pinfo) {
+    if (pdata->next && pdata->next->data && (pinfo = get_protocol(pdata->next->id))) {
         pinfo->print_pdu(buf, n, pdata->next);
     } else {
         PRINT_PROTOCOL(buf, n, "IPv6");