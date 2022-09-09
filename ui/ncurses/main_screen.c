jo-lund/jomon

commit acb10c8edbc28707a34450d3931894be44e3bb1e

ui: Fixed a display bug with goto line

ui/ncurses/main_screen.c
@@ -747,42 +747,34 @@ void main_screen_goto_line(main_screen *ms, int c)
         }
         wrefresh(status);
     } else if (num && (c == '\n' || c == KEY_ENTER)) {
-        if ((ms->subwindow.win && num > ms->subwindow.num_lines + vector_size(ms->packet_ref)) ||
-            (!ms->subwindow.win && num > vector_size(ms->packet_ref))) {
+        if (num > ms->subwindow.num_lines + vector_size(ms->packet_ref))
             return;
-        }
 
         int my;
 
         my = getmaxy(ms->base.win);
-        if (num >= ms->base.top && num < ms->base.top + my) {
-            UPDATE_SELECTIONBAR(ms->base.win, ms->base.selectionbar, BACKGROUND);
+        if (num >= ms->base.top && num < ms->base.top + my - ms->subwindow.num_lines) {
             if (ms->subwindow.win && num > ms->base.top + ms->subwindow.top) {
                 ms->base.selectionbar = num - 1 + ms->subwindow.num_lines;
             } else {
                 ms->base.selectionbar = num - 1;
             }
-            UPDATE_SELECTIONBAR(ms->base.win, ms->base.selectionbar, SELECTIONBAR);
         } else {
-            werase(ms->base.win);
-            UPDATE_SELECTIONBAR(ms->base.win, ms->base.selectionbar, BACKGROUND);
+            if (ms->subwindow.win)
+                delete_subwindow(ms, false);
             if (num + my - 1 > vector_size(ms->packet_ref)) {
-                print_lines(ms, vector_size(ms->packet_ref) - my, vector_size(ms->packet_ref), 0);
                 ms->base.top = vector_size(ms->packet_ref) - my;
                 ms->base.selectionbar = num - 1;
-                UPDATE_SELECTIONBAR(ms->base.win, ms->base.selectionbar - ms->base.top, SELECTIONBAR);
             } else {
-                print_lines(ms, num - 1, num + my - 1, 0);
                 ms->base.selectionbar = ms->base.top = num - 1;
-                UPDATE_SELECTIONBAR(ms->base.win, 0, SELECTIONBAR);
             }
         }
-        wrefresh(ms->base.win);
         curs_set(0);
         werase(status);
         input_mode = INPUT_NONE;
         num = 0;
         actionbar_refresh(actionbar, (screen *) ms);
+        main_screen_refresh((screen *) ms);
     } else if (c == KEY_ESC) {
         curs_set(0);
         werase(status);

BUGS
@@ -1,4 +1,2 @@
 * KNOWN BUGS:
 - Resizing of window causes problems with layout.
-- If you go to a line (with "go to line") on the current screen and the selection
-  line is on bolded text, the text will disappear.

commit e65c623a891282aaf06a0cdda677c1eafa365635

Fixed a regression introduced by acb10c8
main_screen_set_interactive will now call main_screen_refresh to redraw
the entire screen. This should not be done from main_screen_clear.

ui/ncurses/main_screen.c
@@ -219,7 +219,7 @@ void main_screen_clear(main_screen *ms)
     ms->base.selectionbar = 0;
     ms->base.top = 0;
     ms->outy = 0;
-    main_screen_set_interactive(ms, false);
+    ms->base.show_selectionbar = false;
     werase(ms->header);
     werase(ms->base.win);
 }