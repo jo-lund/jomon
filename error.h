f703db908b546ed4b156376d7501bb059f302025
John Olav Lund <john.olav.lund@gmail.com>
2023-02-28 12:03:28 +0100

    Declared err_quit and err_sys as noreturn

--- error.h
+++ error.h
@@ -4,10 +4,10 @@
 #include "attributes.h"
 
 /* Fatal error. Print error message and quit */
-void err_quit(const char *fmt, ...) PRINTF_FORMAT(1, 2);
+void err_quit(const char *fmt, ...) PRINTF_FORMAT(1, 2) NORETURN;
 
 /* Fatal error related to a system call. Print error message and quit */
-void err_sys(const char *fmt, ...) PRINTF_FORMAT(1, 2);
+void err_sys(const char *fmt, ...) PRINTF_FORMAT(1, 2) NORETURN;
 
 /* Non-fatal error. Print error message */
 void err_msg(const char *fmt, ...) PRINTF_FORMAT(1, 2);
