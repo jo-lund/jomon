jo-lund/jomon

commit 419767d6b94f3297894d1ee02d3559719f9475c0

Added documentation

signal.c
@@ -2,15 +2,17 @@
 #include "signal.h"
 #include "list.h"
 
+/* Implementation of a publish/subscribe mechanism */
+
 struct publisher {
     list_t *subscriptions0;
     list_t *subscriptions1;
     list_t *subscriptions2;
 };
 
-publisher_t *publisher_init()
+publisher_t *publisher_init(void)
 {
-    publisher_t *p = xmalloc(sizeof(publisher_t));
+    publisher_t *p = malloc(sizeof(publisher_t));
 
     p->subscriptions0 = list_init(NULL);
     p->subscriptions1 = list_init(NULL);

signal.h
@@ -2,20 +2,41 @@
 #define SIGNAL_H
 
 typedef struct publisher publisher_t;
-typedef void (*publisher_fn0)();
+typedef void (*publisher_fn0)(void);
 typedef void (*publisher_fn1)(void *);
 typedef void (*publisher_fn2)(void *, void *);
 
-publisher_t *publisher_init();
+/* Initialize the publisher. Resources need to be freed by calling publisher_free */
+publisher_t *publisher_init(void);
+
+/* Free reources related to publisher */
 void publisher_free(publisher_t *p);
+
+/* Add subscription with no arguments */
 void add_subscription0(publisher_t *p, publisher_fn0 f);
+
+/* Remove subscription with no arguments */
 void remove_subscription0(publisher_t *p, publisher_fn0 f);
+
+/* Invoke subscribers with no arguments */
 void publish0(publisher_t *p);
+
+/* Add subscription with 1 argument */
 void add_subscription1(publisher_t *p, publisher_fn1 f);
+
+/* Remove subscription with 1 argument */
 void remove_subscription1(publisher_t *p, publisher_fn1 f);
+
+/* Invoke subscribers with 1 argument */
 void publish1(publisher_t *p, void *data);
+
+/* Add subscription with 2 arguments */
 void add_subscription2(publisher_t *p, publisher_fn2 f);
+
+/* Remove subscription with 2 arguments */
 void remove_subscription2(publisher_t *p, publisher_fn2 f);
+
+/* Invoke subscribers with 2 arguments */
 void publish2(publisher_t *p, void *d1, void *d2);
 
 #endif

commit 9c0f40efd87e88f1f07b8f6dd8ae1a8b07aa8cc1

Fixed a regression introduced by 419767d

signal.c
@@ -12,7 +12,7 @@ struct publisher {
 
 publisher_t *publisher_init(void)
 {
-    publisher_t *p = malloc(sizeof(publisher_t));
+    publisher_t *p = xmalloc(sizeof(publisher_t));
 
     p->subscriptions0 = list_init(NULL);
     p->subscriptions1 = list_init(NULL);