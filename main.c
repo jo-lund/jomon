jo-lund/jomon

commit 912d31128d483e8e9355d85d60983f0dd821bf5f

Store network statistics in the backgound
Will now store all network statistics in the background even when the
statistics screen is not shown.

main.c
@@ -302,6 +302,7 @@ static void run(void)
         { STDIN_FILENO, POLLIN, 0 }
     };
 
+    alarm(1);
     while (1) {
         timer_run();
         if (alarm_flag) {

ui/ncurses/connection_screen.c
@@ -65,7 +65,6 @@ static void connection_screen_init(screen *s);
 static void connection_screen_refresh(screen *s);
 static void connection_screen_get_input(screen *s);
 static void connection_screen_got_focus(screen *s, screen *oldscr UNUSED);
-static void connection_screen_lost_focus();
 static unsigned int connection_screen_get_size(screen *s);
 static void connection_screen_on_back(screen *s);
 static void connection_screen_render(connection_screen *cs);
@@ -76,7 +75,6 @@ static screen_operations csop = {
     .screen_refresh = connection_screen_refresh,
     .screen_get_input = connection_screen_get_input,
     .screen_got_focus = connection_screen_got_focus,
-    .screen_lost_focus = connection_screen_lost_focus,
     .screen_get_data_size = connection_screen_get_size,
     .screen_on_back = connection_screen_on_back
 };
@@ -570,14 +568,6 @@ void connection_screen_got_focus(screen *s, screen *oldscr UNUSED)
         }
         active = true;
     }
-    if (ctx.capturing)
-        alarm(1);
-}
-
-void connection_screen_lost_focus()
-{
-    if (ctx.capturing)
-        alarm(0);
 }
 
 void connection_screen_on_back(screen *s UNUSED)

ui/ncurses/stat_screen.c
@@ -63,24 +63,24 @@ static void stat_screen_free(screen *s);
 static void stat_screen_init(screen *s);
 static void stat_screen_get_input(screen *s);
 static void stat_screen_refresh(screen *s);
-static void stat_screen_got_focus();
-static void stat_screen_lost_focus();
 
 static screen_operations ssop = {
     .screen_init = stat_screen_init,
     .screen_free = stat_screen_free,
     .screen_refresh = stat_screen_refresh,
     .screen_get_input = stat_screen_get_input,
-    .screen_got_focus = stat_screen_got_focus,
-    .screen_lost_focus = stat_screen_lost_focus,
 };
 
 static void handle_alarm(void)
 {
     screen *s = (screen *) screen_cache_get(STAT_SCREEN);
 
-    if (s->focus)
+    if (s->focus) {
         stat_screen_print(s);
+    } else {
+        get_netstat(ctx.device, &rx, &tx);
+        calculate_rate();
+    }
 }
 
 screen *stat_screen_create(void)
@@ -133,16 +133,6 @@ void stat_screen_refresh(screen *s)
         memset(cpustat[i], 0, hw.num_cpu * sizeof(struct cputime));
     }
     stat_screen_print(s);
-}
-
-void stat_screen_got_focus()
-{
-    alarm(1);
-}
-
-void stat_screen_lost_focus()
-{
-    alarm(0);
 }
 
 void stat_screen_get_input(screen *s)

commit 37a173bc595f092c4300a363093d333cea4fb0a7

Fixed a regression introduced by 912d3112
Need to handle SIGALRM when using the --count option. The default disposition
for SIGALRM is to terminate the process.

main.c
@@ -212,9 +212,9 @@ int main(int argc, char **argv)
         print_bpf();
     if (!ctx.device && !(ctx.device = get_default_interface()))
         err_quit("Cannot find active network device");
+    setup_signal(SIGALRM, sig_callback, SA_RESTART);
     if (ctx.opt.show_count)
         handle_count_and_exit(buf);
-    setup_signal(SIGALRM, sig_callback, SA_RESTART);
     decoder_init();
     tcp_analyzer_init();
     dns_cache_init();