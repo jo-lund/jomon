04a9af3f4e24ce946c59c57d4bd693e90fb542c6
John Olav Lund <john.olav.lund@gmail.com>
2025-11-01 15:18:01 +0100

    Removed unused struct

--- decoder/packet_llc.h
+++ decoder/packet_llc.h
@@ -3,18 +3,6 @@
 
 #include "packet.h"
 
-/* Ethernet 802.2 Logical Link Control */
-struct eth_802_llc {
-    uint8_t dsap; /* destination service access point */
-    uint8_t ssap; /* source service access point */
-    uint8_t control; /* possible to be 2 bytes? */
-};
-
-#define get_llc(p) ((struct eth_802_llc *)(p)->root->next->data)
-#define llc_dsap(p) get_llc(p)->dsap
-#define llc_ssap(p) get_llc(p)->ssap
-#define llc_control(p) get_llc(p)->control
-
 void register_llc(void);
 
 #endif
